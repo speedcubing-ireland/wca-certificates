fc03f88f0c4fedca9c0ad7456672b5bba8f1aa13
Simon Kelly <26287997+simonkellly@users.noreply.github.com>
2026-01-25 14:02:17 +0000

    Fix environment URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 
 export const environment = {
-  appUrl : 'https://simonkellly.github.io/wca-certificates/',
+  appUrl : 'https://speedcubing-ireland.github.io/wca-certificates/',
   wcaUrl : 'https://www.worldcubeassociation.org',
 
   wcaAppId : 'Hk6Uh0h5Lf8yUchSnwZKw2RO8nTMqnDNGwuTJF-HBOY',
